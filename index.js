b5097e296243173b7b845d5d85e6367568ba66cf
tucksa <tuckerjsarah@gmail.com>
2020-02-18 15:00:14 -0500

    turning date() to string values

--- index.js
+++ index.js
@@ -8,3 +8,70 @@ $("#btn").click(function(){
     }
     
 })
+const date = new Date()
+let month = ''
+const day = date.getDate()
+const year = date.getUTCFullYear()
+let weekday = '';
+switch(date.getDay()){
+    case 0:
+        weekday = 'Sunday'
+        break;
+    case 1:
+        weekday = 'Monday'
+        break;
+    case 2:
+        weekday = 'Tuesday'
+        break;
+    case 3:
+        weekday = 'Wednesday'
+        break;
+    case 4:
+        weekday = 'Thursday'
+        break;
+    case 5:
+        weekday = 'Friday'
+        break;
+    case 6:
+        weekday = 'Saturday'
+}
+switch(date.getMonth() + 1){
+    case 1:
+        month = 'January'
+        break;
+    case 2:
+        month = 'Febuary'
+        break;
+    case 3:
+        month = 'March'
+        break;
+    case 4: 
+        month = 'April'
+        break;
+    case 5:
+        month = 'May'
+        break;
+    case 6:
+        month = 'June'
+        break;
+    case 7:
+        month = 'July'
+        break;
+    case 8:
+        month = 'August'
+        break;
+    case 9:
+        month= 'September'
+        break;
+    case 10:
+        month = 'October'
+        break;
+    case 11:
+        month = 'November'
+        break;
+    case 12:
+        month = 'December'
+}
+
+$('#month').html(month)
+console.log(weekday)
